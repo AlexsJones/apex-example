36c4ede855f90fe973f503c083c563af17a7fa8c
AlexsJones <tibbar@freedommail.ch>
2017-04-30 13:31:58 +0100

    improved example

--- functions/go/main.go
+++ functions/go/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/apex/go-apex"
 )
 
 type message struct {
-	Name string `json:"name"`
+	Count int `json:"count"`
 }
 
 type response struct {
-	Reply string `json:"reply"`
+	Results []string `json:"results"`
 }
 
+func fibGenerator() chan int {
+	c := make(chan int)
+
+	go func() {
+		for i, j := 0, 1; ; i, j = i+j, i {
+			c <- i
+		}
+	}()
+
+	return c
+}
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 		var m message
@@ -21,7 +33,17 @@ func main() {
 		if err := json.Unmarshal(event, &m); err != nil {
 			return nil, err
 		}
-		r := response{"Yo, " + m.Name}
+		c := m.Count
+		c1 := fibGenerator()
+
+		results := make([]string, c)
+		for x := 0; x < c; x++ {
+			answer := strconv.Itoa(<-c1)
+			results = append(results, answer)
+		}
+
+		r := response{Results: results}
+
 		return r, nil
 	})
 }
